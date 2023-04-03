f179c8a37943bb4dd7012f638ca16320294240a6
Lina Kutitskaya <kutitckaya.lina@yandex.ru>
2023-04-03 17:42:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Вам необходимо:
+
+загрузить docker-контейнер по ссылке:
+
+https://hub.docker.com/layers/sirena86/stock_products3/latest/images/sha256-a84cabc2446acccda4101ac456a8a8b0ecc2da8e4cc4bd03489dee4917bebee0?context=repo
+
+и запустить:
+
+docker run -d -p 7777:6777 sirena86/stock_products3:latest
---
+++ logistic/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'products', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+
+        for position in positions:
+            stock_product = StockProduct(
+                stock=stock,
+                product=position.get('product'),
+                quantity=position.get('quantity'),
+                price=position.get('price')
+            )
+            stock_product.save()
+
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().update(instance, validated_data)
+
+        for position in positions:
+            product = position.get('product')
+            quantity = position.get('quantity')
+            price = position.get('price')
+
+            obj, created = StockProduct.objects.update_or_create(
+                stock=stock,
+                product=product,
+                defaults={'price': price,
+                          'quantity': quantity}
+            )
+
+        return stock
